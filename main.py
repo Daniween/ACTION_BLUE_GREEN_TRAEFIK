f8535c8b718167c8ccf125f86b942a2ec8ef0a24
Daniween <soudry.daniel99@gmail.com>
2025-11-20 12:06:52 +0100

    add health route

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+@app.get("/health")
+async def health():
+    return JSONResponse(content={"status": "ok"}, status_code=200)
 
 if __name__ == "__main__":
     import uvicorn
